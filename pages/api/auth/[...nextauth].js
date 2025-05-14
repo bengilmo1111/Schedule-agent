b2d8cd599aee2eb1723c3b585d0be6f89d0eaa39
bengilmo1111 <bengilmo@gmail.com>
2025-05-14 23:11:48 +1200

    nextauth improvements

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,41 +1,42 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
-  // Configure one or more authentication providers
+export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId:     process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: [
-            "openid",
-            "email",
-            "profile",
-            "https://www.googleapis.com/auth/calendar.freebusy",
-            "https://www.googleapis.com/auth/calendar.events",
-            "https://www.googleapis.com/auth/gmail.send",
-            "https://www.googleapis.com/auth/gmail.modify"
-          ].join(" ")
+          prompt:       "consent",
+          access_type:  "offline",
+          response_type:"code",
+          scope: `
+            openid
+            email
+            profile
+            https://www.googleapis.com/auth/gmail.modify
+            https://www.googleapis.com/auth/gmail.labels
+            https://www.googleapis.com/auth/calendar.events
+          `.trim().replace(/\s+/g," ")
         }
       }
     })
   ],
   callbacks: {
-    // Persist the access and refresh tokens in the JWT
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
+        token.accessToken  = account.access_token;
         token.refreshToken = account.refresh_token;
       }
       return token;
     },
-    // Make the tokens available in the session object
     async session({ session, token }) {
-      session.user.accessToken = token.accessToken;
+      session.user.accessToken  = token.accessToken;
       session.user.refreshToken = token.refreshToken;
       return session;
     }
   }
-});
+};
+
+export default NextAuth(authOptions);
